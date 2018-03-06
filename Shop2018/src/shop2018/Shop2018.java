2ec00403065c4c995083c60e632e96e71a59a6de
Student <Student@193.107.34.204>
2018-03-06 13:18:35 +0100

    Poprawka na bigDecimal

--- Shop2018/src/shop2018/Shop2018.java
+++ Shop2018/src/shop2018/Shop2018.java
@@ -1,15 +1,38 @@
 
 package shop2018;
 
+import java.math.BigDecimal;
+
 
 
 public class Shop2018 {
 
   
-    
-    
+ 
     public static void main(String[] args) {
 
+        
+       Produkt chleb =new Produkt("chleb",new BigDecimal("2.2"));
+       Produkt maslo=new Produkt("maslo",new BigDecimal("6.15"));
+       Produkt mleko=new Produkt("mleko",new BigDecimal("1.5"));
+       Produkt jogurt =new Produkt("jogurt",new BigDecimal("0.95"));
+       
+       
+       Koszyk koszyk = new Koszyk();
+       koszyk.dodaj(chleb);
+       koszyk.dodaj(maslo);
+       koszyk.dodaj(mleko);
+       koszyk.dodaj(jogurt);
+       
+       System.out.println();
+       System.out.println("Cena wynosi:"+koszyk.doZaplaty()+"zł");
+               
+       
+       
+
+        
+        
+        
         
         
     }
