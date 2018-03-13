f9987f9d92196cff7702f9a10a8764a1f8a9761e
Student <Student@193.107.34.204>
2018-03-13 12:42:50 +0100

    Zmiana nazw refaktoryzacja

---
+++ Shop2018/src/shop2018/Product.java
@@ -0,0 +1,28 @@
+package shop2018;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public String toString()
+    {
+        return name +"("+price+"zł";
+    }
+    
+    
+    
+}
